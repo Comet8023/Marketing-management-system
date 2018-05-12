8eb3a5706e01b2c17fe044ee27d48ba6d583ec69
Comet8023 <liuwenhan.comet@qq.com>
2018-05-13 04:16:20 +0800

    修改综合查询页面

--- src/routes/ReportManagement/OverallPage.js
+++ src/routes/ReportManagement/OverallPage.js
@@ -1,97 +1,87 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { PureComponent } from 'react';
 import moment from 'moment';
-import {
-  Row,
-  Col,
-  Card,
-  Form,
-  Input,
-  Select,
-  Icon,
-  Button,
-  Dropdown,
-  Menu,
-  InputNumber,
-  DatePicker,
-  Modal,
-  message,
-  Badge,
-  Divider,
-} from 'antd';
-import {Table} from 'antd';
+import 'moment/locale/zh-cn';
+import {Form, Input, DatePicker, Button, Table} from 'antd';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 
-
+const FormItem = Form.Item;
+const dateFormat = 'YYYY/MM/DD';
+@Form.create()
 export default class OverallPage extends PureComponent {
-    
-      render() {
-        const columns = [
-          {
-            title: '用户名',
-            dataIndex: 'customerName',
-          },
-          {
-            title: 'A种气源',
-            dataIndex: 'AGasSrc',
-          },
-          {
-            title: 'B种气源',
-            dataIndex: 'BGasSrc',
-          },
-          {
-            title: 'C种气源',
-            dataIndex: 'CGasSrc',
-          },
-          {
-            title: 'D种气源',
-            dataIndex: 'DGasSrc',
-          },
-          {
-            title: 'E种气源',
-            dataIndex: 'EGasSrc',
-          },
-          {
-            title: '管输费',
-            dataIndex: 'pipesRate',
-          }, 
-          {
-            title: '用气量',
-            dataIndex: 'usedGas',
-          },
-          {
-            title: '总利润',
-            dataIndex: 'sumProfit',
-          }, 
-        ];
-
-        const dataSource = [{
-          key: '1',
-          customerName: '客户1',
-          AGasSrc: 1,
-          BGasSrc: 1,
-          CGasSrc: 1,
-          DGasSrc: 1,
-          EGasSrc: 1,
-          pipesRate: 1,
-          usedGas: 1,
-          sumProfit: 1
-        }, {
-          key: '2',
-          customerName: '客户2',
-          AGasSrc: 2,
-          BGasSrc: 2,
-          CGasSrc: 2,
-          DGasSrc: 2,
-          EGasSrc: 2,
-          pipesRate: 2,
-          usedGas: 2,
-          sumProfit: 2
-        }];
-
-        return (
-          <PageHeaderLayout>
-            <Table dataSource={dataSource} columns={columns}/>
-        </PageHeaderLayout>
-        );
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const columns = [
+      {
+        title: '用户名称',
+        dataIndex: 'userName',
+      },
+      {
+        title: '气源均价',
+        dataIndex: 'buyAveragePrice',
+      },
+      {
+        title: '销售均价',
+        dataIndex: 'saleAveragePrice',
+      },
+      {
+        title: '成本',
+        dataIndex: 'totalCost',
+      },
+      {
+        title: '管输费',
+        dataIndex: 'totalFee',
+      },
+      {
+        title: '销售收入',
+        dataIndex: 'totalIncome',
       }
+    ];
+
+    const dataSource = [
+      {
+        key: '1',
+        userName: '客户1',
+        buyAveragePrice: 1,
+        saleAveragePrice: 1,
+        totalCost: 1,
+        totalFee: 1,
+        totalIncome: 1
+      }, 
+      {
+        key: '2',
+        userName: '客户1',
+        buyAveragePrice: 1,
+        saleAveragePrice: 1,
+        totalCost: 1,
+        totalFee: 1,
+        totalIncome: 1
+      }
+    ];
+
+    const handleSubmit = e => {      
+      e.preventDefault();     
+      this.props.form.validateFields((err, values) => {
+        if (!err) {
+          alert(values);
+          
+        }
+      });
+    }    
+    
+    return (
+      <PageHeaderLayout >
+        <Form layout="inline">
+          <FormItem>
+            <DatePicker allowClear={false} defaultValue={moment(new Date().toLocaleDateString(), dateFormat)} format={dateFormat} />
+          </FormItem>
+          <FormItem>
+              <Button type="primary" htmlType="submit" onClick={handleSubmit}>
+                查询
+              </Button>
+          </FormItem>
+        </Form>
+        <Table dataSource={dataSource} columns={columns}/>
+      </PageHeaderLayout>
+    );
+  }
 }
